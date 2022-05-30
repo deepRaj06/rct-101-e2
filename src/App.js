b3f21c88f9ef16a0f1dd8b504347ee8d58285eeb
deepRaj06git config --global user.name deepRaj06 <deepak.onlinework13@gmail.com>
2022-05-30 16:58:23 +0530

    pending evaluation due to access denied to port 8080

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Products from './components/Products';
+import prodData from '../db.json'
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+
+  const [product, setProduct] = useState(prodData)
+  return <div>
+    {/* TODO: Code here */}
+    {product.map((prod) => {
+          <Products>{prod}</Products>
+    })}
+    </div>;
 };
 
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import axios from'axios';
+import AddProduct from "./AddProduct";
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+  const[prod, setProd] = useState([]);
+  const[page, setPage] = useState(1);
+  const[limit, setLimit] = useState(3);
+
+  useEffect( () => {
+    axios.get(`http://localhost:8000/products?_page=${Number(page)}&_limit=${Number(limit)}`)
+    .then( (res) => {
+      console.log(res);
+      setProd(res.data);
+    })
+  })
 
   return (
     <Flex>
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      <AddProduct></AddProduct>
+      <Grid>
+        {/* List of Products */}
+        </Grid>
       {/* Pagination */}
     </Flex>
   );
